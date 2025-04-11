53d213a2346234e807a0861b96ac2e9d5bc3fdf3
Sally Alazab <142817956+Sally2026@users.noreply.github.com>
2025-04-11 16:06:00 +0000

    Working on the project

---
+++ project/assets/js/script.js
@@ -0,0 +1,28 @@
+$(document).ready(function () {
+    $('.slider').slick({
+      centerMode: true,
+      centerPadding: '60px',
+      slidesToShow: 4,
+      responsive: [
+        {
+          breakpoint: 768,
+          settings: {
+            arrows: false,
+            centerMode: true,
+            centerPadding: '40px',
+            slidesToShow: 2
+          }
+        },
+        {
+          breakpoint: 480,
+          settings: {
+            arrows: false,
+            centerMode: true,
+            centerPadding: '40px',
+            slidesToShow: 1
+          }
+        }
+      ]
+    });
+  });
+  
